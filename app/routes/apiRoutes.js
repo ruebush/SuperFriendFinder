a5f753c00889d560764c638461b4ee758fad166d
ruebush <jamesruebush@gmail.com>
2019-02-28 20:58:01 -0500

    j f c

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,12 +1,7 @@
 // dependencies
 var friends = require("../data/friends.js");
-var path = require("path");
 
 module.exports = function(app) {
-
-    app.get("/"), function (res, req) {
-        res.json(friends);
-    };
 
     app.get("/api/friends", function (req, res) {
         res.json(friends);
